0d7bb998b67cd1e82f14e76bae3658f16b138029
gluglu553 <hugoomartins2005@outlook.com>
2025-09-06 16:11:34 -0300

    2

---
+++ app/views/Home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <div>
+       <link rel="import" href="NavBar.php">
+    </div>
+    <div>
+        <link rel="import" href="Footer.php">
+            <h1> Bem-Vindo a Paróquia </h1>
+            <h1>São Pedro Apóstolo</h1>
+    </div>
+<input type="image" src="../../public/assets/images/padroeiro.jpeg">
+    <container>
+        <h1></h1>
+    </container>
+</body>
+</html>
