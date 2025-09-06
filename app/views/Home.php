f913852af762bc111931e12854d4abeeb2b00176
gluglu553 <hugoomartins2005@outlook.com>
2025-09-06 16:48:09 -0300

    HomeMapAdd

--- app/views/Home.php
+++ app/views/Home.php
@@ -4,19 +4,24 @@
     <meta charset="UTF-8">
     <title>Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <css rel="stylesheet" href="../../public/assets/css/Home.css">
 </head>
 <body>
     <div>
        <link rel="import" href="NavBar.php">
     </div>
-    <div>
-        <link rel="import" href="Footer.php">
+
+    <div id="HomePG1">
             <h1> Bem-Vindo a Paróquia </h1>
-            <h1>São Pedro Apóstolo</h1>
+            <h1>São Judas Tadeu</h1>
+<input id="SaoJudas" type="image" src="../../public/assets/images/padroeiro.jpeg">
     </div>
-<input type="image" src="../../public/assets/images/padroeiro.jpeg">
+
     <container>
-        <h1></h1>
+        <h1>onde estamos</h1>
+        <map>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.632573622634!2d-46.55397368502164!3d-23.601648984686803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5a5e8b0f6b7b%3A0x8e6f3c4b8c4e4e0a!2sPar%C3%B3quia%20S%C3%A3o%20Judas%20Tadeu!5e0!3m2!1spt-BR!2sbr!4v1696061234567!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </container>
+
 </body>
 </html>
