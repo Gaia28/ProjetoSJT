fc6aa4ea74e17588158a02c222bcf2a2b131c606
Gaia28 <matheusgaia33@gmail.com>
2025-09-08 14:53:50 -0300

    REFACTOR | admin login refatoração do html e estilo

--- app/views/AdminLogin.php
+++ app/views/AdminLogin.php
@@ -7,18 +7,24 @@
     <title>Administração</title>
 </head>
 <body>
-    <img src="../../public/assets/images/logo.PNG" alt="logo">
-    <h1>Administração SJT</h1>
-    <div class="login-container">
-        <form action="/admin/login" method="POST">
-            <label for="username">Usuário:</label>
-            <input type="text" id="username" name="username" required>
-
-            <label for="password">Senha:</label>
-            <input type="password" id="password" name="password" required>
-
-            <button type="submit">Entrar</button>
-        </form>
-
-    </div>
+    <main class="container" role="main">
+        <header>
+            <img src="../../public/assets/images/logo.PNG" alt="Logo do Santuário São Judas Tadeu">
+            <h1>Administração <br>São Judas Tádeu</h1>
+        </header>
+        <section class="login-container" aria-label="Formulário de login administrativo">
+            <form action="/admin/login" method="POST" autocomplete="username">
+                <div class="form-group">
+                    <label for="username">Usuário:</label>
+                    <input type="text" id="username" name="username" required autocomplete="username">
+                </div>
+                <div class="form-group">
+                    <label for="password">Senha:</label>
+                    <input type="password" id="password" name="password" required autocomplete="current-password">
+                </div>
+                <button type="submit">Entrar</button>
+            </form>
+        </section>
+    </main>
+</body>
 </html>
