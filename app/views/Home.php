e3175b5b5ebab629d3d8b9dc44a1b1867d5a0d10
gluglu553 <hugoomartins2005@outlook.com>
2025-09-09 18:20:13 -0300

    #cssHome

--- app/views/Home.php
+++ app/views/Home.php
@@ -20,7 +20,7 @@
 
     <section class="Pages" id="HomePG2">
         <h1>Paróquia Santuário <br>São Judas Tadeu</h1>
-            <img src="public/assets/images/fachada.jpeg" alt="">
+            <img class="img" src="public/assets/images/fachada.jpeg" alt="">
             <map id="mapa">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.632573622634!2d-46.55397368502164!3d-23.601648984686803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5a5e8b0f6b7b%3A0x8e6f3c4b8c4e4e0a!2sPar%C3%B3quia%20S%C3%A3o%20Judas%20Tadeu!5e0!3m2!1spt-BR!2sbr!4v1696061234567!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></br>
         <linkwebsite href="">Facebook</linkwebsite>
@@ -63,7 +63,7 @@
     </section>
     <section id="HomePG5">
         <h1>sobre nós</h1></br>
-        <img src="public/assets/images/cristo.jpeg" alt="">
+        <img class="img" src="public/assets/images/cristo.jpeg" alt="">
         <p>A Paróquia São Judas Tadeu é uma comunidade vibrante e acolhedora, dedicada a promover a fé, a esperança e o amor entre seus membros. Fundada em [ano de fundação], nossa paróquia tem sido um pilar espiritual na região, oferecendo uma variedade de serviços religiosos, programas educacionais e atividades comunitárias.</p>
         <button>saiba mais</button>
     <section>  
