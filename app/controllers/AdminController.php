c882f081a471063b056e35b9b3f77f1ca9e91826
Gaia28 <matheusgaia33@gmail.com>
2025-09-09 21:37:02 -0300

    REFACTOR | correções nas telas de admin

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,17 +1,12 @@
 <?php
-
 use Exception;
- 
 
 if($_SERVER['REQUEST_METHOD']=== 'POST'){
     $usuario = $_POST['username'] ?? '';
     $senha = $_POST['password'] ?? '';
 
     if($usuario === 'admin' && $senha === '123456'){
-
-        session_start();
-        $_SESSION['admin_logged_in'] = true;
-        header('Location: /admin/dashboard');
+        header('Location: /views/HomeAdmin.php');
         exit();
     } else {
         // Credenciais inválidas
--- app/views/HomeAdmin.php
+++ app/views/HomeAdmin.php
@@ -6,17 +6,45 @@ use App\controllers\AdminController;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/assets/css/Admin.css">
     <title>Administração</title>
 </head>
 <body>
-    <header>
+    <main class="containerHome" role="main"> 
+        <header class="header">
          <div class="circle"><img src="public/assets/images/logo.PNG" alt="Logo do Santuário"></div>
-            <p>Santuário São Judas Tadeu</p>
+            <p>Paróquia Santuário São Judas Tadeu</p>
             <nav>
                 <a href="#">Sair</a>
                 
             </nav>
     </header>
+
+    <section class="home-content">
+        <h1>Bem-vindo ao Painel Administrativo</h1>
+        <p>Gerencie o conteúdo do site aqui.</p>
+
+        <div class ="cards">
+            <div class="card">
+                <h2>Gerenciar Notícias</h2>
+                <p>Adicione, edite ou remova notícias do site.</p>
+               
+            </div>
+            <div class="card">
+                <h2>Gerenciar Eventos</h2>
+                <p>Adicione, edite ou remova eventos do calendário.</p>
+            </div>
+            <div class="card">
+                <h2>Gerenciar Usuários</h2>
+                <p>Controle os usuários que têm acesso ao painel administrativo.</p>
+            </div>
+            <div class="card">
+                <h2>Sacaramentos</h2>
+                <p>Cadastre os sacramentos realizados no santuário.</p>
+
+        </div>
+    </section>
+    </main>
 </body>
 
 </html>
