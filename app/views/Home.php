ea19786444f02ff5651e5ba8bd68fcaa412ceaad
gluglu553 <hugoomartins2005@outlook.com>
2025-09-15 16:59:12 -0300

    #css

--- app/views/Home.php
+++ app/views/Home.php
@@ -20,9 +20,8 @@
 
     <section class="Pages" id="HomePG2">
         <h1>Paróquia Santuário <br>São Judas Tadeu</h1>
-            <img class="img" src="../../public/assets/images/fachada.jpeg" alt="">
-            <map id="mapa">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.632573622634!2d-46.55397368502164!3d-23.601648984686803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5a5e8b0f6b7b%3A0x8e6f3c4b8c4e4e0a!2sPar%C3%B3quia%20S%C3%A3o%20Judas%20Tadeu!5e0!3m2!1spt-BR!2sbr!4v1696061234567!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></br>
+                <iframe id="Mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.632573622634!2d-46.55397368502164!3d-23.601648984686803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5a5e8b0f6b7b%3A0x8e6f3c4b8c4e4e0a!2sPar%C3%B3quia%20S%C3%A3o%20Judas%20Tadeu!5e0!3m2!1spt-BR!2sbr!4v1696061234567!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></br>
+        <img class="img" id="fachada" src="../../public/assets/images/fachada.jpeg" alt="">
         <linkwebsite href="">Facebook</linkwebsite>
         <linkwebsite href="">Instagram</linkwebsite>
         <linkwebsite href="">WhatsApp</linkwebsite>
@@ -39,21 +38,21 @@
         <h1>horarios</h1></br>
         <p>Participe de nossas celebrações e fortaleça sua fé em comunidade</p></br>
 
-        <article>
+        <article id="artigo1">
             <icon></icon>
             <h2>missas dominicais</h2>
             <p>dia:</p>
             <p>horário:</p>
         </article>
 
-        <article>
+        <article id="artigo2">
             <icon></icon>
             <h2>missas semanais</h2>
             <p>dia:</p>
             <p>horário:</p>
         </article>
 
-        <article>
+        <article id="artigo3">
             <icon></icon>
             <h2>louvor e adoração</h2>
             <p>dia:</p>
@@ -70,7 +69,5 @@
 </main>
 
 </body>
-//
-
 </html>
 
