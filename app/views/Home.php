da8e86ab2da2b5736236d1f0df465956c9c4435c
gluglu553 <hugoomartins2005@outlook.com>
2025-09-15 17:27:08 -0300

    #css

--- app/views/Home.php
+++ app/views/Home.php
@@ -19,22 +19,28 @@
     <?php include 'NavBar.php'; ?>
 
     <section class="Pages" id="HomePG2">
-        <h1>Paróquia Santuário <br>São Judas Tadeu</h1>
+        <div class="linha"> 
+            <h1>Paróquia Santuário <br>São Judas Tadeu</h1>
                 <iframe id="Mapa" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.632573622634!2d-46.55397368502164!3d-23.601648984686803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5a5e8b0f6b7b%3A0x8e6f3c4b8c4e4e0a!2sPar%C3%B3quia%20S%C3%A3o%20Judas%20Tadeu!5e0!3m2!1spt-BR!2sbr!4v1696061234567!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></br>
         <img class="img" id="fachada" src="../../public/assets/images/fachada.jpeg" alt="">
         <linkwebsite href="">Facebook</linkwebsite>
         <linkwebsite href="">Instagram</linkwebsite>
         <linkwebsite href="">WhatsApp</linkwebsite>
+        </div>
     </section>
 
     <section id="HomePG3">
+        <div class="linha"> 
         <h1>transmissões</h1></br>
         <p>acompanhe-nos ao vivo no youtube</p></br>
         <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/live_stream?channel=YOUR_CHANNEL_ID" title="YouTube live stream" 
-            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></br>    
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></br>  
+        </div>  
          </section>
 
     <section id="HomePG4">
+<div class="linha">        
+        
         <h1>horarios</h1></br>
         <p>Participe de nossas celebrações e fortaleça sua fé em comunidade</p></br>
 
@@ -58,14 +64,16 @@
             <p>dia:</p>
             <p>horário:</p>
         </article>
-
+</div>
     </section>
     <section id="HomePG5">
+        <div class="linha">
         <h1>sobre nós</h1></br>
         <img class="img" src="../../public/assets/images/cristo.jpeg" alt="">
         <p>A Paróquia São Judas Tadeu é uma comunidade vibrante e acolhedora, dedicada a promover a fé, a esperança e o amor entre seus membros. Fundada em [ano de fundação], nossa paróquia tem sido um pilar espiritual na região, oferecendo uma variedade de serviços religiosos, programas educacionais e atividades comunitárias.</p>
-        <button>saiba mais</button>
-    <section>  
+        <button class="botoes">saiba mais</button>
+    <section> 
+        </div> 
 </main>
 
 </body>
