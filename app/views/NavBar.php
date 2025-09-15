5b02a1e2a0c14040fd7e417065c26f1b8a892e46
gluglu553 <hugoomartins2005@outlook.com>
2025-09-15 16:23:56 -0300

    Correção

--- app/views/NavBar.php
+++ app/views/NavBar.php
@@ -2,13 +2,13 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="public/assets/css/NavBar.css">
+    <link rel="stylesheet" href="../../public/assets/css/NavBar.css">
     <title>Navigation Bar</title>
 
 </head>
 <body>
     <header>
-            <div class="circle"><img src="public/assets/images/logo.PNG" alt="Logo do Santuário"></div>
+            <div class="circle"><img src="../../public/assets/images/logo.PNG" alt="Logo do Santuário"></div>
             <p>Santuário São Judas Tadeu</p>
             <div class="menu-toggle" id="menu-toggle">&#9776;</div>
             <nav>
