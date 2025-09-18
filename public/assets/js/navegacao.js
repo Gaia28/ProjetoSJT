8d520212ef9c5fdd85ff80524fffc567e476552b
Gaia28 <matheusgaia33@gmail.com>
2025-09-18 19:35:59 -0300

    FEAT | novos redirecionamento

--- public/assets/js/navegacao.js
+++ public/assets/js/navegacao.js
@@ -13,3 +13,18 @@ cards.forEach(card => {
         });
     }
 });
+
+const redesSociais = [
+    { id: 'facebook', url: 'https://www.facebook.com/paroquiasaojosedojaguare' },
+    { id: 'instagram', url: 'https://www.instagram.com/sjtbelem?igsh=aHo0MWJkbTJoZ3U5' },
+    { id: 'youtube', url: 'https://www.youtube.com/channel/UCV8tq1H3b7kXJ4h2bX5F4mA' }
+];
+
+redesSociais.forEach(rede => {
+    const element = document.getElementById(rede.id);
+    if (element) {
+        element.addEventListener('click', () => {  
+        window.open(rede.url, '_blank');
+        });
+    }
+});
