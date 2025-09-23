d19c468dff9a2d9cbaf943c5ca43232fee790369
Gaia28 <matheusgaia33@gmail.com>
2025-09-23 17:52:01 -0300

    FEAT | rota das redes sociais

--- public/assets/js/navegacao.js
+++ public/assets/js/navegacao.js
@@ -15,9 +15,9 @@ cards.forEach(card => {
 });
 
 const redesSociais = [
-    { id: 'facebook', url: 'https://www.facebook.com/paroquiasaojosedojaguare' },
+    { id: 'facebook', url: 'https://www.facebook.com/share/1F8TDq5ui3/' },
     { id: 'instagram', url: 'https://www.instagram.com/sjtbelem?igsh=aHo0MWJkbTJoZ3U5' },
-    { id: 'youtube', url: 'https://www.youtube.com/channel/UCV8tq1H3b7kXJ4h2bX5F4mA' }
+    { id: 'whatsapp', url: 'https://api.whatsapp.com/send?phone=%2B559131156020&fbclid=IwVERDUAM_XpdleHRuA2FlbQIxMAABHpZIiLmAbO6WuyF2V002scb7y63wP5js9ohScUO8wSuyVBmN_Y_z-HVuu_Hz_aem_Fv4CknDfFkCB6sK4oaqjrQ' }
 ];
 
 redesSociais.forEach(rede => {
