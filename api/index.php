d283cc6d1368ca07135b8a444d312f98931e98fb
Gaia28 <matheusgaia33@gmail.com>
2025-10-13 21:06:33 -0300

    #

---
+++ api/index.php
@@ -0,0 +1,6 @@
+<?php
+
+// Este arquivo serve como uma "ponte" para a Vercel.
+// Ele simplesmente inclui e executa o seu arquivo index.php principal.
+phpinfo();
+require __DIR__ . '/../index.php';
